bc4881470a735e14079d936519ba45abf11b483e
kou2911 <pnonkiti@icloud.com>
2021-09-26 17:56:07 +0900

    ユーザー管理昨日の単体テストの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,10 +8,11 @@ class User < ApplicationRecord
   validates :birthday,           presence: true
 
   with_options presence: true do
-    validates :last_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters."}
-    validates :first_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters."}
-    validates :last_name_reading, format: {with: /\A[ァ-ヶー]+\z/, message: "is invalid. Input full-width katakana characters."}
-    validates :first_name_reading, format: {with: /\A[ァ-ヶー]+\z/, message: "is invalid. Input full-width katakana characters."}
- 
+    validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters.' }
+    validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters.' }
+    validates :last_name_reading, format: { with: /\A[ァ-ヶー]+\z/, message: 'is invalid. Input full-width katakana characters.' }
+    validates :first_name_reading, format: { with: /\A[ァ-ヶー]+\z/, message: 'is invalid. Input full-width katakana characters.' }
+    PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+    validates_format_of :password, with: PASSWORD_REGEX
   end
 end
---
+++ spec/factories/users.rb
@@ -0,0 +1,15 @@
+FactoryBot.define do
+  factory :user do
+    nickname { Faker::Name.name }
+    email    { Faker::Internet.free_email }
+
+    password { '1a' + Faker::Internet.password(min_length: 6) }
+
+    password_confirmation { password }
+    last_name { '吉本' }
+    first_name { '康二' }
+    last_name_reading { 'ヨシモト' }
+    first_name_reading { 'コウジ' }
+    birthday { Faker::Date.between(from: '1930-01-01', to: '2016-12-31') }
+  end
+end
