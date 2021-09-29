f875eb7decb82728503e0788a52ea99e56dc29ef
kou2911 <pnonkiti@icloud.com>
2021-09-29 21:28:43 +0900

    商品データの保存

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,11 +11,11 @@ class Item < ApplicationRecord
 
   validates :item_name, presence: true
   validates :text     , presence: true
-  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :status_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :sipping_day_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :delivery_fee_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :category_id, numericality: { other_than: 0 , message: "can't be blank"}
+  validates :status_id, numericality: { other_than: 0 , message: "can't be blank"}
+  validates :sipping_day_id, numericality: { other_than: 0 , message: "can't be blank"}
+  validates :delivery_fee_id, numericality: { other_than: 0 , message: "can't be blank"}
+  validates :prefecture_id, numericality: { other_than: 0 , message: "can't be blank"}
   with_options presence: true, format: { with: /\A[0-9]+\z/ } do
     validates :price, numericality: {only_integer: true, greater_than_or_equal_to:300, less_than_or_equal_to:9999999},
                presence: true
