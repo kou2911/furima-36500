b76b89599919c3e8c8596932299df9ecb0078a8b
kou2911 <pnonkiti@icloud.com>
2021-10-01 22:48:31 +0900

    itemモデルの単体テスト作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,7 +8,8 @@ class Item < ApplicationRecord
   belongs_to :delivery_fee
   belongs_to :sipping_day
   belongs_to :prefecture
-
+  
+  validates :image, presence: true
   validates :item_name, presence: true
   validates :text     , presence: true
   validates :category_id, numericality: { other_than: 0 , message: "can't be blank"}
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :item do
-    
+    item_name        {Faker::Name.name}
+    text             {Faker::Lorem.sentence}
+    category_id      {1}
+    status_id        {1}
+    delivery_fee_id  {1}
+    prefecture_id    {1}
+    sipping_day_id   {1} 
+    price            {1000}
+    association :user
+
+    after(:build) do |image|
+      image.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
