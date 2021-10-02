6a7b65a6414dbf42323cae04749700e7ea5a89e4
kou2911 <pnonkiti@icloud.com>
2021-10-02 21:30:05 +0900

    商品情報編集機能修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new,:edit]
+  before_action :find, only:[:show,:edit,:update]
   before_action :move_index, only: [:edit]
 
   def index
@@ -20,15 +21,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path(@item.id)
     else
@@ -39,14 +37,16 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:image, :item_name, :text, :category_id, :status_id, :delivery_fee_id, :prefecture_id, :sipping_day_id,
-                                 :price).merge(user_id: current_user.id)
+    params.require(:item).permit(:image, :item_name, :text, :category_id, :status_id, :delivery_fee_id, :prefecture_id, :sipping_day_id,:price).merge(user_id: current_user.id)
   end
 
   def move_index
-    @item = Item.find(params[:id])
     unless @item.user.id == current_user.id
       redirect_to action: :index 
     end
+  end
+
+  def find
+    @item =Item.find(params[:id])
   end
 end
